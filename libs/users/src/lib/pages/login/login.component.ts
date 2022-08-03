7f0f9d027457820366cc2185719e0c6c3d4b7cc4
Jose <josebratt@gmail.com>
2022-08-03 10:35:43 -0400

    Create login.component.ts

---
+++ libs/users/src/lib/pages/login/login.component.ts
@@ -0,0 +1,73 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { LocalstorageService } from '../../services/localstorage.service';
+
+@Component({
+  selector: 'users-login',
+  templateUrl: './login.component.html',
+  styles: [],
+})
+export class LoginComponent implements OnInit {
+  loginFormGroup!: FormGroup;
+  isSubmitted = false;
+  authError = false;
+  authMessage = 'Email or Password are wrong';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private auth: AuthService,
+    private localstorageService: LocalstorageService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this._initLoginForm();
+  }
+
+  private _initLoginForm() {
+    this.loginFormGroup = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      passwordHash: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+
+    if (this.loginFormGroup.invalid) return;
+    
+    this.auth
+      .login(
+        this.formControl['email'].value,
+        this.formControl['passwordHash'].value
+      )
+      .subscribe({
+        next: (user) => {
+          console.log('el frontend envia: ', user);
+          
+          this.authError = false;
+          this.localstorageService.setToken(user.token);
+          this.router.navigate(['/']);
+        },
+        error: (error: HttpErrorResponse) => {
+          this.authError = true;
+          if (error.status !== 400) {
+            this.authMessage = 'Error in the Server, please try again later!';
+          }
+        },
+      });
+  }
+
+  /** abreviation instruction */
+  get formControl(): { [key: string]: AbstractControl } {
+    return this.loginFormGroup.controls;
+  }
+}
