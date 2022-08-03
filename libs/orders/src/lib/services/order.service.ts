bc997eca24b2a95f116ad381b5ebf3d37b4fc8a6
Jose <josebratt@gmail.com>
2022-08-03 08:04:48 -0400

    Create order.service.ts

---
+++ libs/orders/src/lib/services/order.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Order } from '@ecommerce/orders';
+import { environment } from '@env/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  
+  apiURLOrders = environment.apiURL + 'orders';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getOrders(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.apiURLOrders);
+  }
+
+  getOrder(orderId: string): Observable<Order> {
+    return this.http.get<Order>(`${this.apiURLOrders}/${orderId}`);
+  }
+
+  createOrder(order: Order): Observable<Order> {
+    return this.http.post<Order>(this.apiURLOrders, order);
+  }
+
+  updateOrder(orderStaus: { status: string }, orderId: string): Observable<Order> {
+    return this.http.put<Order>(`${this.apiURLOrders}/${orderId}`, orderStaus);
+  }
+
+  deleteOrder(orderId: string): Observable<Order> {
+    return this.http.delete<Order>(`${this.apiURLOrders}/${orderId}`);
+  }
+}
