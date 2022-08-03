920ddcb95f4b309abf47b99742089f19d01e42b8
Jose <josebratt@gmail.com>
2022-08-03 08:01:28 -0400

    Create sidebar.component.ts

---
+++ apps/admin/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: [
+  ]
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  logoutUser(){}
+
+}
