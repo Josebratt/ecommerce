3411c478d4d88e2f5d45e8b7b2178bd36641e1cb
Jose <josebratt@gmail.com>
2022-08-03 11:05:46 -0400

    Update users-form.component.ts

--- apps/admin/src/app/pages/users/users-form/users-form.component.ts
+++ apps/admin/src/app/pages/users/users-form/users-form.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Location } from '@angular/common';
 import { MessageService } from 'primeng/api';
 import { ActivatedRoute } from '@angular/router';
 import { User, UsersService } from '@ecommerce/users';
 
-import * as countriesLib from "i18n-iso-countries";
+import * as countriesLib from 'i18n-iso-countries';
 import { timer, takeUntil, Subject } from 'rxjs';
 declare const require: (arg0: string) => countriesLib.LocaleData;
 
 @Component({
   selector: 'admin-users-form',
   templateUrl: './users-form.component.html',
-  styles: []
+  styles: [],
 })
-export class UsersFormComponent implements OnInit {
-
+export class UsersFormComponent implements OnInit, OnDestroy {
   form!: FormGroup;
   isSubmitted = false;
   editmode = false;
@@ -29,12 +33,17 @@ export class UsersFormComponent implements OnInit {
     private usersService: UsersService,
     private location: Location,
     private route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this._initUserForm();
     this._checkEditMode();
     this._getcontries();
+  }
+
+  ngOnDestroy(): void {
+    this.endsubs$.next(true);
+    this.endsubs$.complete();
   }
 
   private _initUserForm() {
@@ -48,7 +57,7 @@ export class UsersFormComponent implements OnInit {
       apartment: [''],
       zip: [''],
       city: [''],
-      country: ['']
+      country: [''],
     });
   }
 
@@ -60,15 +69,15 @@ export class UsersFormComponent implements OnInit {
     const user: User = {
       id: this.currentUserId,
       name: this.formControl['name'].value,
-      passwordHash:this.formControl['password'].value,
-      email:this.formControl['email'].value,
-      phone:this.formControl['phone'].value,
-      isAdmin:this.formControl['isAdmin'].value,
-      street:this.formControl['street'].value,
-      apartment:this.formControl['apartment'].value,
-      zip:this.formControl['zip'].value,
-      city:this.formControl['city'].value,
-      country:this.formControl['country'].value,
+      passwordHash: this.formControl['password'].value,
+      email: this.formControl['email'].value,
+      phone: this.formControl['phone'].value,
+      isAdmin: this.formControl['isAdmin'].value,
+      street: this.formControl['street'].value,
+      apartment: this.formControl['apartment'].value,
+      zip: this.formControl['zip'].value,
+      city: this.formControl['city'].value,
+      country: this.formControl['country'].value,
     };
     if (this.editmode) {
       this._updateUser(user);
@@ -78,38 +87,42 @@ export class UsersFormComponent implements OnInit {
   }
 
   private _addUser(user: User) {
-    this.usersService.createUser(user).pipe(takeUntil(this.endsubs$)).subscribe({
-      next: (user) => {
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Success Message',
-          detail: `User ${user.name} is created`,
-        });
-        timer(2000)
-          .pipe(takeUntil(this.endsubs$))
-          .subscribe({ next: () => this.location.back() });
-      },
-      error: () => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error Message',
-          detail: `User is not created`,
-        });
-      }
-    }
-    );
+    this.usersService
+      .createUser(user)
+      .pipe(takeUntil(this.endsubs$))
+      .subscribe({
+        next: (user) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success Message',
+            detail: `User ${user.name} is created`,
+          });
+          timer(2000)
+            .pipe(takeUntil(this.endsubs$))
+            .subscribe({ next: () => this.location.back() });
+        },
+        error: () => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error Message',
+            detail: `User is not created`,
+          });
+        },
+      });
   }
 
   /**/
   private _checkEditMode() {
     let id = '';
-    this.route.params.subscribe(
-      (params) => {
-        id = params['id']
-        if (id) {
-          this.editmode = true;
-          this.currentUserId = id;
-          this.usersService.getUser(id).pipe(takeUntil(this.endsubs$)).subscribe((user) => {
+    this.route.params.pipe(takeUntil(this.endsubs$)).subscribe((params) => {
+      id = params['id'];
+      if (id) {
+        this.editmode = true;
+        this.currentUserId = id;
+        this.usersService
+          .getUser(id)
+          .pipe(takeUntil(this.endsubs$))
+          .subscribe((user) => {
             this.formControl['name'].setValue(user.name);
             this.formControl['email'].setValue(user.email);
             this.formControl['phone'].setValue(user.phone);
@@ -123,31 +136,33 @@ export class UsersFormComponent implements OnInit {
             this.formControl['password'].setValidators([]);
             this.formControl['password'].updateValueAndValidity();
           });
-        }
-      });
+      }
+    });
   }
 
   private _updateUser(user: User) {
-    this.usersService.updateUser(user).pipe(takeUntil(this.endsubs$)).subscribe({
-      next: (user) => {
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Success Message',
-          detail: `User ${user.name} is updated`,
-        });
-        timer(2000)
-          .pipe(takeUntil(this.endsubs$))
-          .subscribe({ next: () => this.location.back() });
-      },
-      error: () => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error Message',
-          detail: `User is not updated`,
-        });
-      }
-    }
-    );
+    this.usersService
+      .updateUser(user)
+      .pipe(takeUntil(this.endsubs$))
+      .subscribe({
+        next: (user) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success Message',
+            detail: `User ${user.name} is updated`,
+          });
+          timer(2000)
+            .pipe(takeUntil(this.endsubs$))
+            .subscribe({ next: () => this.location.back() });
+        },
+        error: () => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error Message',
+            detail: `User is not updated`,
+          });
+        },
+      });
   }
 
   onCancel() {
@@ -156,20 +171,19 @@ export class UsersFormComponent implements OnInit {
 
   //
   private _getcontries() {
-    countriesLib.registerLocale(require("i18n-iso-countries/langs/en.json"));
-    this.countries = Object.entries(countriesLib.getNames("en", { select: "official" })).map(
-      (entry) => {
-        return {
-          id: entry[0],
-          name: entry[1]
-        }
-      }
-    );
+    countriesLib.registerLocale(require('i18n-iso-countries/langs/en.json'));
+    this.countries = Object.entries(
+      countriesLib.getNames('en', { select: 'official' })
+    ).map((entry) => {
+      return {
+        id: entry[0],
+        name: entry[1],
+      };
+    });
   }
 
-    /** abreviation instruction */
-    get formControl(): { [key: string]: AbstractControl } {
-      return this.form.controls;
-    }
-
+  /** abreviation instruction */
+  get formControl(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
 }
