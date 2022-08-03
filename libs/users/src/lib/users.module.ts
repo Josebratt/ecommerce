06ebb8dd1490fc7246908522f9869abd03e094b9
Jose <josebratt@gmail.com>
2022-08-03 10:36:01 -0400

    Update users.module.ts

--- libs/users/src/lib/users.module.ts
+++ libs/users/src/lib/users.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { LoginComponent } from './pages/login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule],
+  declarations: [
+    LoginComponent
+  ],
 })
 export class UsersModule {}
