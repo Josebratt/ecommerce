be7a07030cf8abdc1ce6aec8233c824a80495b08
Jose <josebratt@gmail.com>
2022-08-03 08:03:13 -0400

    Update app.module.ts

--- apps/eshop/src/app/app.module.ts
+++ apps/eshop/src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PrimengModule } from './primeng/primeng.module';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+]
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomePageComponent, HeaderComponent, FooterComponent],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    PrimengModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
