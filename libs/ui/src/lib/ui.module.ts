37fa44461301f4b456e94f89be2e2e432a9d0615
Jose <josebratt@gmail.com>
2022-08-04 12:33:43 -0400

    Update ui.module.ts

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
+import { BannerComponent } from './components/banner/banner.component';
+
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [ButtonModule],
+  exports: [
+    BannerComponent
+  ],
+  declarations: [
+    BannerComponent
+  ]
 })
 export class UiModule {}
