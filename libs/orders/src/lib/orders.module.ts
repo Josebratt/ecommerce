c191fb85ea83ebed02211993f03c57968f26ed81
Jose <josebratt@gmail.com>
2022-08-06 11:01:02 -0400

    Update orders.module.ts

--- libs/orders/src/lib/orders.module.ts
+++ libs/orders/src/lib/orders.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CartIconComponent } from './components/cart-icon/cart-icon.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    CartIconComponent
+  ],
+  exports: [
+    CartIconComponent
+  ],
 })
 export class OrdersModule {}
