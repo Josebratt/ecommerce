afe2055b4ea4dc58d761b83ffa693ede38047cda
Jose <josebratt@gmail.com>
2022-08-06 16:43:29 -0400

    Update cart.service.ts

--- libs/orders/src/lib/services/cart.service.ts
+++ libs/orders/src/lib/services/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Cart, CartItem } from '../models/cart';
 
 export const CART_KEY = 'cart';
@@ -7,21 +8,30 @@ export const CART_KEY = 'cart';
   providedIn: 'root',
 })
 export class CartService {
+
+  cart$: BehaviorSubject<Cart> = new BehaviorSubject(this.getCart());
+  
   /** we initial cart */
   initCartLocalStorage() {
     const cart: Cart = this.getCart();
-    /** we validate that cart doesn't have nothing */
-    if (!cart) {
-      /** Here we initialise cart with nothing */
-      const intialCart = {
-        items: [],
-      };
-      /** we parse the intialCart to string */
-      const intialCartJson = JSON.stringify(intialCart);
-
-      /** Here we set Item into localstore */
-      localStorage.setItem(CART_KEY, intialCartJson);
+    if (Cart) {
+      this.emptyCart();
     }
+
+    /** validate that cart doesn't have nothing */
+   // if (!cart) {
+      /** Here initialise cart with nothing */
+      // const intialCart = {
+      //   items: [],
+      // };
+      /** parse the intialCart to string */
+      // const intialCartJson = JSON.stringify(intialCart);
+
+      /** Here set Item into localstore */
+    //   localStorage.setItem(CART_KEY, intialCartJson);
+    // } else {
+    //   this.cart$.next(cart);
+    // }
   }
 
   /** here we get the item from the localstore */
@@ -34,22 +44,32 @@ export class CartService {
     const cart = this.getCart();
 
     /** we validate if the cartItemId is the same that itemProductId to increase the quantity */
-    const cartItemExist = cart.items.find(
+    const cartItemExist = cart.items?.find(
       (item) => item.productId === cartItem.productId
     );
     if (cartItemExist) {
-      cart.items.map((item) => {
+      cart.items?.map((item) => {
         if (item.productId === cartItem.productId) {
           item.quantity = item.quantity + cartItem.quantity;
         }
       });
     } else {
-      cart.items.push(cartItem);
+      cart.items?.push(cartItem);
     }
 
     /** before you push cartItems to cart you need to send the information to localstore*/
     const cartJson = JSON.stringify(cart);
     localStorage.setItem(CART_KEY, cartJson);
+    this.cart$.next(cart);
     return cart;
+  }
+
+  emptyCart() {
+    const intialCart = {
+      items: []
+    };
+    const intialCartJson = JSON.stringify(intialCart);
+    localStorage.setItem(CART_KEY, intialCartJson);
+    this.cart$.next(intialCart);
   }
 }
