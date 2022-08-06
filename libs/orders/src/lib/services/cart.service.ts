a027b6d9d4fc7dc7b466fa582247a33285323a34
Jose <josebratt@gmail.com>
2022-08-06 11:01:06 -0400

    Update cart.service.ts

--- libs/orders/src/lib/services/cart.service.ts
+++ libs/orders/src/lib/services/cart.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+export const CART_KEY = 'cart';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  /** we initial cart */
+  initCartLocalStorage() {
+    const intialCart = {
+      items: []
+    };
+    const intialCartJson = JSON.stringify(intialCart);
+    localStorage.setItem(CART_KEY, intialCartJson);
+  }
 }
