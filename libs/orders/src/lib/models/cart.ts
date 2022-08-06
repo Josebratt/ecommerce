c069328d77432ce60925d748b931807e807f23a1
Jose <josebratt@gmail.com>
2022-08-06 10:30:26 -0400

    Create cart.ts

---
+++ libs/orders/src/lib/models/cart.ts
@@ -0,0 +1,13 @@
+export class Cart {
+  items: CartItem[] = [];
+}
+
+export class CartItem {
+  productId = '';
+  quantity = 0;
+}
+
+export class CartItemDetailed {
+  product?: any;
+  quantity = 0;
+}
