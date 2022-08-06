509087922ca71f6acfca6f0e79b829c9d55c9cd2
Jose <josebratt@gmail.com>
2022-08-06 11:57:04 -0400

    Update orders.module.ts

--- libs/orders/src/lib/orders.module.ts
+++ libs/orders/src/lib/orders.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { BadgeModule } from 'primeng/badge';
 import { CartIconComponent } from './components/cart-icon/cart-icon.component';
 import { CartService } from './services/cart.service';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [
-    CartIconComponent
-  ],
-  exports: [
-    CartIconComponent
-  ],
+  imports: [CommonModule, BadgeModule],
+  declarations: [CartIconComponent],
+  exports: [CartIconComponent],
 })
 export class OrdersModule {
-  constructor(
-    cartService: CartService
-  ) {
+  constructor(cartService: CartService) {
     cartService.initCartLocalStorage();
   }
 }
