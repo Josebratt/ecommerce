8570010a9fcc9c9d19fb39e2edd7e6802efb89fb
Jose <josebratt@gmail.com>
2022-08-07 13:35:27 -0400

    Update orders.module.ts

--- libs/orders/src/lib/orders.module.ts
+++ libs/orders/src/lib/orders.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
-import { BadgeModule } from 'primeng/badge';
-import { CartIconComponent } from './components/cart-icon/cart-icon.component';
 import { CartService } from './services/cart.service';
 
+import { BadgeModule } from 'primeng/badge';
+import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
+
+import { CartIconComponent } from './components/cart-icon/cart-icon.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+
+
+
+const routes: Routes = [
+  { path: 'cart', component: CartPageComponent },
+]
+
 @NgModule({
-  imports: [CommonModule, BadgeModule],
-  declarations: [CartIconComponent],
-  exports: [CartIconComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), BadgeModule, ButtonModule, InputNumberModule],
+  declarations: [CartIconComponent, CartPageComponent],
+  exports: [CartIconComponent, CartPageComponent],
 })
 export class OrdersModule {
   constructor(cartService: CartService) {
