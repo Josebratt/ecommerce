21faaf95fee6721926187233b595ca02e566d63b
Jose <josebratt@gmail.com>
2022-08-07 14:58:53 -0400

    Update cart-page.component.ts

--- libs/orders/src/lib/pages/cart-page/cart-page.component.ts
+++ libs/orders/src/lib/pages/cart-page/cart-page.component.ts
@@ -1,5 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { CartItemDetailed, CartService, OrderService } from '@ecommerce/orders';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'orders-cart-page',
@@ -9,18 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPageComponent implements OnInit {
 
+  cartItemDetailed: CartItemDetailed[] = [];
+
+  endSubs$: Subject<any> = new Subject();
+
   constructor(
-    private router: Router
+    private router: Router,
+    private cartService: CartService,
+    private orderService: OrderService
   ) { }
 
   ngOnInit(): void {
+    this._getCartDetails();
+  }
+
+  private _getCartDetails() {
+    this.cartService.cart$.pipe().subscribe(
+      respCart => {                
+        respCart.items.forEach(
+          cartItem => this.orderService.getProduct(cartItem.productId).subscribe(
+            (resProducts) => {
+              this.cartItemDetailed.push({
+                product: resProducts,
+                quantity: cartItem.quantity
+              })
+            }
+            
+          )
+          
+        )
+      }
+    )
   }
 
   backToShop() {
     this.router.navigate(['/products']);
   }
   deleteCartItem(){
-    
+
   }
 
 }
