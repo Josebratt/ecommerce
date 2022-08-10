7348eec8777e6aa345629a4a675c1a99dbc8b1c6
Jose <josebratt@gmail.com>
2022-08-10 16:31:38 -0400

    Update users.service.ts

--- libs/users/src/lib/services/users.service.ts
+++ libs/users/src/lib/services/users.service.ts
@@ -3,17 +3,19 @@ import { Injectable } from '@angular/core';
 import { environment } from '@env/environment';
 import { Observable } from 'rxjs';
 import { User } from '../models/users';
+import * as countriesLib from 'i18n-iso-countries';
+declare const require: (arg0: string) => countriesLib.LocaleData;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-
   apiURLUsers = environment.apiURL + 'users';
+  countries: unknown[] = [];
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {
+    countriesLib.registerLocale(require('i18n-iso-countries/langs/en.json'));
+  }
 
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.apiURLUsers);
@@ -33,5 +35,20 @@ export class UsersService {
 
   deleteUser(userId: string): Observable<User> {
     return this.http.delete<User>(`${this.apiURLUsers}/${userId}`);
+  }
+
+  getCountries(): { id: string; name: string }[] {
+    return (this.countries = Object.entries(
+      countriesLib.getNames('en', { select: 'official' })
+    ).map((entry) => {
+      return {
+        id: entry[0],
+        name: entry[1],
+      };
+    }));
+  }
+
+  getCountry(countryKey: string): string {
+    return countriesLib.getName(countryKey, 'en');
   }
 }
