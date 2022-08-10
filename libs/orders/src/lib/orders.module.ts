cad9d0f4fb66d6098e96d0d4b24a08ecc4f067ce
Jose <josebratt@gmail.com>
2022-08-10 15:49:14 -0400

    Update orders.module.ts

--- libs/orders/src/lib/orders.module.ts
+++ libs/orders/src/lib/orders.module.ts
@@ -1,4 +1,6 @@
-import { FormsModule } from '@angular/forms';
+import { InputMaskModule } from 'primeng/inputmask';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
@@ -9,10 +11,12 @@ import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
 import { InputNumberModule } from 'primeng/inputnumber';
 
+
 import { CartIconComponent } from './components/cart-icon/cart-icon.component';
 import { CartPageComponent } from './pages/cart-page/cart-page.component';
 import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
 import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
+import { InputTextModule } from 'primeng/inputtext';
 
 const routes: Routes = [
   { path: 'cart', component: CartPageComponent },
@@ -23,10 +27,14 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes),
     BadgeModule,
     ButtonModule,
+    InputMaskModule,
     InputNumberModule,
+    InputTextModule,
+    DropdownModule
   ],
   declarations: [
     CartIconComponent,
