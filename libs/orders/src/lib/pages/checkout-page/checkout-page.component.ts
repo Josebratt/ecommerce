89c441801024a09597719df0373171910267f959
Jose <josebratt@gmail.com>
2022-08-10 15:49:23 -0400

    Update checkout-page.component.ts

--- libs/orders/src/lib/pages/checkout-page/checkout-page.component.ts
+++ libs/orders/src/lib/pages/checkout-page/checkout-page.component.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { OrderItem } from '@ecommerce/orders';
 
 @Component({
   selector: 'orders-checkout-page',
   templateUrl: './checkout-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class CheckoutPageComponent implements OnInit {
+  checkoutFormGroup!: FormGroup;
+  isSubmitted = false;
+  orderItems: OrderItem[] = [];
+  userId = '609d65943373711346c5e950';
+  countries = [];
+  formBuilder: any;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this._initCheckoutForm();
   }
 
+  private _initCheckoutForm() {
+    this.checkoutFormGroup = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      phone: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      zip: ['', Validators.required],
+      apartment: ['', Validators.required],
+      street: ['', Validators.required],
+    });
+  }
+
+  placeOrder() {}
+
+  backToCart() {
+    this.router.navigate(['/cart']);
+  }
+
+  get formControl() {
+    return this.checkoutFormGroup.controls;
+  }
 }
