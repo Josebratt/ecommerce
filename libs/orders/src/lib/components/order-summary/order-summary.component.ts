03bc811593010f7902531e831ed3ee530b2321cb
Jose <josebratt@gmail.com>
2022-08-10 09:30:44 -0400

    Create order-summary.component.ts

---
+++ libs/orders/src/lib/components/order-summary/order-summary.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService, OrderService } from '@ecommerce/orders';
+import { Subject, take } from 'rxjs';
+
+@Component({
+  selector: 'orders-order-summary',
+  templateUrl: './order-summary.component.html',
+  styles: [],
+})
+export class OrderSummaryComponent implements OnInit, OnDestroy {
+  endSubs$: Subject<any> = new Subject();
+  totalPrice = 0;
+  isCheckout = false;
+
+  constructor(
+    private router: Router,
+    private cartService: CartService,
+    private orderService: OrderService
+  ) {}
+
+  ngOnInit(): void {
+    this._getOrderSummary();
+  }
+
+  ngOnDestroy(): void {
+    this.endSubs$.next(true);
+    this.endSubs$.complete();
+  }
+
+  private _getOrderSummary() {
+    this.cartService.cart$.subscribe((cart) => {
+      this.totalPrice = 0;
+      if (cart) {
+        cart.items.map((item) => {
+          this.orderService
+            .getProduct(item.productId)
+            .pipe(take(1))
+            .subscribe((product) => {
+              this.totalPrice += product.price * item.quantity;
+            });
+        });
+      }
+    });
+  }
+
+  navigateToCheckout() {
+    this.router.navigate(['/checkout']);
+  }
+}
