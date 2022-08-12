a1ec10429364f6bc99f02e71ddde6849c718568c
Jose <josebratt@gmail.com>
2022-08-12 15:16:25 -0400

    Update app.component.ts

--- apps/eshop/src/app/app.component.ts
+++ apps/eshop/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { UsersService } from '@ecommerce/users';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'eshop-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eshop';
+
+  constructor(
+    private usersService: UsersService
+  ) {}
+
+  ngOnInit(): void {
+    this.usersService.initAppSession();
+  }
 }
