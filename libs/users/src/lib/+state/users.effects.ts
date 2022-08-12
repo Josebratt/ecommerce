68e4ea05517e3d4549d5dcc555eea662bb120a94
Jose <josebratt@gmail.com>
2022-08-12 15:16:42 -0400

    Update users.effects.ts

--- libs/users/src/lib/+state/users.effects.ts
+++ libs/users/src/lib/+state/users.effects.ts
@@ -1,27 +1,40 @@
+import { UsersService } from '@ecommerce/users';
+import { LocalstorageService } from './../services/localstorage.service';
 import { Injectable } from '@angular/core';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { fetch } from '@nrwl/angular';
+import { catchError, map, mergeMap, of } from 'rxjs';
 
 import * as UsersActions from './users.actions';
-import * as UsersFeature from './users.reducer';
 
 @Injectable()
 export class UsersEffects {
-  init$ = createEffect(() =>
+  buildUserSession$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(UsersActions.initUsers),
-      fetch({
-        run: (action) => {
-          // Your custom service 'load' logic goes here. For now just return a success action...
-          return UsersActions.loadUsersSuccess({ users: [] });
-        },
-        onError: (action, error) => {
-          console.error('Error', error);
-          return UsersActions.loadUsersFailure({ error });
-        },
+      ofType(UsersActions.buildUserSession),
+      mergeMap(() => {
+        if (this.localstorageService.isValidToken()) {
+          const userId = this.localstorageService.getUserIdFromToken();
+          if (userId) {
+            return  this.usersService.getUser(userId).pipe(
+              map((user) => {
+                return UsersActions.buildUserSessionSuccess({ user });
+              }),
+              catchError(() =>
+                of(UsersActions.buildUserSessionFailure()))
+            );
+          } else {
+            return of(UsersActions.buildUserSessionFailure());
+          }
+        } else {
+          return of(UsersActions.buildUserSessionFailure());
+        }
       })
     )
   );
 
-  constructor(private readonly actions$: Actions) {}
+  constructor(
+    private readonly actions$: Actions,
+    private localstorageService: LocalstorageService,
+    private usersService: UsersService
+  ) {}
 }
